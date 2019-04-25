f2d5d4246cd867810376b173ba1eeeb79ac0ccd6
Anton Plancke <plancke.anton@gmail.com>
2019-04-25 10:54:41 +0200

    Add 'createMap' function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,18 @@ document.addEventListener("DOMContentLoaded", init);
 
 let map;
 let LouvainLaNeuve = [50.667656, 4.614325];
+let zoomLvl = 15;
 
 function init() {
-    map = L.map('mapid').setView(LouvainLaNeuve, 13);
+    createMap();
+    
+    let data = '[{"coords":{"lat": "50.665901", "lon": "4.612952"}, "date": "2019-04-25 10:45"}, {"coords":{"lat": "50.665901", "lon": "4.612952"}, "date": "2019-04-25 10:30"}]';
+
+    displayData(JSON.parse(data));
+}
+
+function createMap() {
+    map = L.map('mapid').setView(LouvainLaNeuve, zoomLvl);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
                 attribution: 'Made by Group 5',
@@ -12,15 +21,11 @@ function init() {
                 id: 'mapbox.streets',
                 accessToken: 'pk.eyJ1Ijoid291dGVycGFyZG9uIiwiYSI6ImNqdXdjM2ZtbzA1MmgzeXBnMzBhMHZiMmMifQ.s1ZmAn_KOd9X3Hlm2b1z_g'
             }).addTo(map);
-
-    let data = [{coords:{lat:50.68, lon:4.62}, datetime:"2019-04-05 10:30"},{coords:{lat:50.59, lon:4.60}, datetime:"2019-04-05 10:25"}]
-
-    displayData(data);
 }
 
 function displayData(data) {
     for (let i = data.length; i > 0; i--) {
-        marker = addMarker(data[i].coords, "Seen on " + data[i].datetime)
+        marker = addMarker(data[i].coords, "Seen on " + data[i].date)
     }
 
     map.setView(data[0]);
